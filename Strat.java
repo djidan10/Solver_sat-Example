0ca70fb4fdd72df19a45883e84184b2185b10d64
Hichem ABDELLALI <hichemabdellali@hotmail.fr>
2015-07-02 18:24:49 +0100

    Create Strat.java

---
+++ Strat.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+
+public class Strat {
+
+	float poid;
+	
+	
+	
+	ArrayList<String> Liste_Clause =new ArrayList<String>(); 
+
+	
+	
+	public void add_Clause(String value)
+	{
+		this.Liste_Clause.add( value);
+	}
+	
+	
+	
+	
+}
+
